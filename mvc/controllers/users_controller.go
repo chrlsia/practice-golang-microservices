35888ee15d4af277ebfc79e587647dead7a80da4
Chris Siannas <xrsia@otenet.gr>
2022-10-24 12:14:34 +0300

    Get user_id

--- mvc/controllers/users_controller.go
+++ mvc/controllers/users_controller.go
@@ -1,7 +1,11 @@
 package controllers
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func GetUser(resp http.ResponseWriter, req *http.Request) {
-
+	userId := req.URL.Query().Get("user_id")
+	log.Printf("about to process user id %v", userId)
 }
