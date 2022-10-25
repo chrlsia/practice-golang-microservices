5cbc2031a44b2f42e474d63ecb61a116962df0b8
Chris Siannas <xrsia@otenet.gr>
2022-10-25 16:22:54 +0300

    assert for the rest

--- mvc/domain/user_dao_test.go
+++ mvc/domain/user_dao_test.go
@@ -12,6 +12,8 @@ func TestGetUserNoUserFound(t *testing.T) {
 	assert.Nil(t, user, "we were not expecting a user with id 0")
 	assert.NotNil(t, err, "we were expecting an error when user id is 0")
 	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not found", err.Code)
+	assert.EqualValues(t, "user 0 was not found", err.Message)
 
 	//if user != nil {
 	//	t.Errorf("we were not expecting a user with id 0")
