f0e417223a9988739ac30bb7bb19d1570a1f812c
Chris Siannas <xrsia@otenet.gr>
2022-10-25 16:18:11 +0300

    assert.NotNil, assert.EqualValues

--- mvc/domain/user_dao_test.go
+++ mvc/domain/user_dao_test.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"github.com/stretchr/testify/assert"
 	"net/http"
 	"testing"
 )
@@ -8,20 +9,28 @@ import (
 func TestGetUserNoUserFound(t *testing.T) {
 	user, err := GetUser(0)
 
-	if user != nil {
-		t.Errorf("we were not expecting a user with id 0")
-	}
+	assert.Nil(t, user, "we were not expecting a user with id 0")
+	assert.NotNil(t, err, "we were expecting an error when user id is 0")
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
 
-	if err == nil {
-		t.Errorf("we were expecting an error when user id is 0")
-	}
+	//if user != nil {
+	//	t.Errorf("we were not expecting a user with id 0")
+	//}
 
-	if err.StatusCode != http.StatusNotFound {
-		t.Errorf("we were expectin 404 when user was not found")
-	}
+	//if err == nil {
+	//	t.Errorf("we were expecting an error when user id is 0")
+	//}
+
+	//if err.StatusCode != http.StatusNotFound {
+	//	t.Errorf("we were expectin 404 when user was not found")
+	//}
 }
 
 /*
 go test
 go test -cover
+
+------
+go get -u github.com/stretchr/testify/assert
+import "github.com/stretchr/testify/assert"
 */
