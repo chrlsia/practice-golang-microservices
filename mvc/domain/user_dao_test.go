65bf38eaa86df7600f8fc2cea7a8a92253e260ca
Chris Siannas <xrsia@otenet.gr>
2022-10-25 18:19:38 +0300

    Second Test function

--- mvc/domain/user_dao_test.go
+++ mvc/domain/user_dao_test.go
@@ -7,8 +7,12 @@ import (
 )
 
 func TestGetUserNoUserFound(t *testing.T) {
+	//Initialization
+
+	//execution
 	user, err := GetUser(0)
 
+	//validation
 	assert.Nil(t, user, "we were not expecting a user with id 0")
 	assert.NotNil(t, err, "we were expecting an error when user id is 0")
 	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
@@ -26,6 +30,18 @@ func TestGetUserNoUserFound(t *testing.T) {
 	//if err.StatusCode != http.StatusNotFound {
 	//	t.Errorf("we were expectin 404 when user was not found")
 	//}
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 123, user.Id)
+	assert.EqualValues(t, "Federico", user.FirstName)
+	assert.EqualValues(t, "Leon", user.LastName)
+	assert.EqualValues(t, "myemail@gmail.com", user.Email)
+
 }
 
 /*
