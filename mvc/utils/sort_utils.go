b3914b3031c370047142eeeffb742ad86d4bb3d1
Chris Siannas <xrsia@otenet.gr>
2022-10-26 11:14:37 +0300

    BubbleSort added

---
+++ mvc/utils/sort_utils.go
@@ -0,0 +1,19 @@
+package utils
+
+//[]int{9,8,7,6,5}
+//[]int{5,6,7,8,9}
+
+func BubbleSort(elements []int) []int {
+	keepRunning := true
+	for keepRunning {
+		keepRunning = false
+		for i := 0; i < len(elements); i++ {
+			if elements[i] > elements[i+1] {
+				elements[i], elements[i+1] = elements[i+1], elements[i]
+				keepRunning = true
+			}
+		}
+
+	}
+	return elements
+}
