57aa71e82cc94167a6e974cd9be757a6c2a29b1b
Chris Siannas <xrsia@otenet.gr>
2022-10-27 14:04:58 +0300

    Test for BubbleSort pass,minor changes

--- mvc/utils/sort_utils.go
+++ mvc/utils/sort_utils.go
@@ -7,7 +7,7 @@ func BubbleSort(elements []int) []int {
 	keepRunning := true
 	for keepRunning {
 		keepRunning = false
-		for i := 0; i < len(elements); i++ {
+		for i := 0; i < len(elements)-1; i++ {
 			if elements[i] > elements[i+1] {
 				elements[i], elements[i+1] = elements[i+1], elements[i]
 				keepRunning = true
