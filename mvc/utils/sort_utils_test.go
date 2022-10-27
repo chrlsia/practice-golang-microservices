e2744115c6634dba7d818ac16b3d73211092a130
Chris Siannas <xrsia@otenet.gr>
2022-10-27 14:16:15 +0300

    WorstCase,BestCase, NilSlice,

--- mvc/utils/sort_utils_test.go
+++ mvc/utils/sort_utils_test.go
@@ -5,10 +5,12 @@ import (
 	"testing"
 )
 
-func TestBubbleSort(t *testing.T) {
+func TestBubbleSortWorstCase(t *testing.T) {
+	//Initialization:
 	els := []int{9, 8, 7, 6, 5}
+	//Execution:
 	BubbleSort(els)
-
+	//Validation:
 	assert.NotNil(t, els)
 	assert.EqualValues(t, 5, len(els))
 	assert.EqualValues(t, 5, els[0])
@@ -16,5 +18,24 @@ func TestBubbleSort(t *testing.T) {
 	assert.EqualValues(t, 7, els[2])
 	assert.EqualValues(t, 8, els[3])
 	assert.EqualValues(t, 9, els[4])
+}
+func TestBubbleSortWBestCase(t *testing.T) {
+	//Initialization:
+	els := []int{5, 6, 7, 8, 9}
+	//Execution:
+	BubbleSort(els)
+	//Validation:
+	assert.NotNil(t, els)
+	assert.EqualValues(t, 5, len(els))
+	assert.EqualValues(t, 5, els[0])
+	assert.EqualValues(t, 6, els[1])
+	assert.EqualValues(t, 7, els[2])
+	assert.EqualValues(t, 8, els[3])
+	assert.EqualValues(t, 9, els[4])
+}
+
+func TestBubbleSortNilSlice(t *testing.T) {
+
+	BubbleSort(nil)
 
 }
