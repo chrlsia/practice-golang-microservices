3c0870ed092a26cebeb7c6310846800a9fe3a286
Chris Siannas <xrsia@otenet.gr>
2022-10-27 14:25:27 +0300

    getElements and TestGetElements

--- mvc/utils/sort_utils_test.go
+++ mvc/utils/sort_utils_test.go
@@ -39,3 +39,25 @@ func TestBubbleSortNilSlice(t *testing.T) {
 	BubbleSort(nil)
 
 }
+
+func getElements(n int) []int {
+	result := make([]int, n)
+	i := 0
+	for j := n - 1; j >= 0; j-- {
+		result[i] = j
+		i++
+	}
+	return result
+}
+
+func TestGetElements(t *testing.T) {
+	els := getElements(5)
+	assert.NotNil(t, els)
+	assert.EqualValues(t, 5, len(els))
+	assert.EqualValues(t, 4, els[0])
+	assert.EqualValues(t, 3, els[1])
+	assert.EqualValues(t, 2, els[2])
+	assert.EqualValues(t, 1, els[3])
+	assert.EqualValues(t, 0, els[4])
+
+}
